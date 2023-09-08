fa046af912f87da4e5280ddd93826010e48784e7
PedroBSanchez <pedro_b47@hotmail.com>
2023-09-08 01:22:11 -0300

    feat : adding validation class to validate request formats

--- backend/src/validations/ProductValidations.ts
+++ backend/src/validations/ProductValidations.ts
@@ -1,18 +1,19 @@
 import Joi from "joi";
 
-import { InterfaceProduct } from "../interfaces/ProductsInterface";
+import {
+  InterfaceProduct,
+  InterfaceProductUpdateRequest,
+} from "../interfaces/ProductsInterface";
 class ProductValidation {
-  public validateProduct(product: InterfaceProduct) {
-    let { code, name, cost_price, sales_price } = product;
+  public validateProduct(product: InterfaceProductUpdateRequest) {
+    let { code, sales_price } = product;
 
     const schema = Joi.object({
       code: Joi.number().required(),
-      name: Joi.string().required(),
-      cost_price: Joi.number().required(),
       sales_price: Joi.number().required(),
     });
 
-    return schema.validate({ code, name, cost_price, sales_price });
+    return schema.validate({ code, sales_price });
   }
 }
 

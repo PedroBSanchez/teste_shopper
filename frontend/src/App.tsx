eb786ca9e0d4c49a1d60e1fd16e6430de0fdb5ac
PedroBSanchez <pedro_b47@hotmail.com>
2023-09-08 01:22:43 -0300

    feat : adding axios request to validate and update products

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,10 +4,20 @@ import "./App.css";
 import Header from "./components/Header";
 import isValidCsvLine from "./shared/isValidCsvLine";
 import Swal from "sweetalert2";
+import axios from "axios";
+
+import { MdWarning } from "react-icons/md";
+import { BiSolidErrorCircle } from "react-icons/bi";
+import { AiFillCheckCircle } from "react-icons/ai";
+
+const apiUrl = "http://localhost:8080";
+
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
 
 interface UpdateProductInterface {
-  product_code: number;
-  new_price: number;
+  code: number;
+  sales_price: number;
 }
 
 function App() {
@@ -15,19 +25,23 @@ function App() {
   const [csvData, setCsvData] = useState<Array<UpdateProductInterface> | null>(
     null
   );
+  const [products, setProducts] = useState<Array<any>>([]);
 
   const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
+    const reader = new FileReader();
 
     if (file) {
-      const reader = new FileReader();
       reader.onload = (e) => {
         const contents = e.target?.result as string;
         const lines = contents.split("\n").map((line) => line.split(","));
         let arrayUpdateProducts: Array<UpdateProductInterface> = [];
         const indexCsvLineProductCode = 0;
         const indexCsvLineNewPrice = 1;
-        for (let index = 1; index < lines.length - 1; index++) {
+        for (let index = 1; index < lines.length; index++) {
+          if (index == lines.length - 1 && lines[index].length == 1) {
+            break;
+          }
           if (
             !isValidCsvLine(
               lines[index][indexCsvLineProductCode],
@@ -42,22 +56,90 @@ function App() {
             break;
           } else {
             arrayUpdateProducts.push({
-              product_code: parseInt(lines[index][indexCsvLineProductCode]),
-              new_price: parseFloat(lines[index][indexCsvLineNewPrice]),
+              code: parseInt(lines[index][indexCsvLineProductCode]),
+              sales_price: parseFloat(lines[index][indexCsvLineNewPrice]),
             });
           }
         }
+        let arrayTmp: any = [];
+        arrayUpdateProducts.forEach((product: any) => {
+          arrayTmp.push({
+            code: product.code,
+            sales_price: product.sales_price,
+            validation: 0,
+          });
+        });
         setCsvData(arrayUpdateProducts);
+        setProducts(arrayTmp);
       };
       reader.readAsText(file);
     }
   };
 
   const handleValidate = async () => {
-    console.log(csvData);
+    if (csvData == null || csvData.length <= 0) {
+      return Swal.fire({
+        text: "Arquivo CSV inválido",
+        icon: "warning",
+      });
+    }
+    const options = {
+      url: `${apiUrl}/api/products/validate`,
+      method: "POST",
+      headers: {
+        ContentType: "application/json",
+      },
+      data: {
+        products: csvData,
+      },
+    };
+    await axios
+      .request(options)
+      .then((response: any) => {
+        if (response.data.valid) {
+          setDisabledUpdate(false);
+        }
+
+        setProducts(response.data.products);
+      })
+      .catch((error: any) => {
+        Swal.fire({
+          text: error.response.data.error || "Falha ao verificar produtos",
+          icon: "warning",
+        });
+      });
   };
 
-  const handleUpdate = async () => {};
+  const handleUpdate = async () => {
+    const options = {
+      url: `${apiUrl}/api/products/update`,
+      method: "PUT",
+      headers: {
+        ContentType: "application/json",
+      },
+      data: {
+        products: csvData,
+      },
+    };
+
+    await axios
+      .request(options)
+      .then((response: any) => {
+        Swal.fire({
+          text: response.data.success,
+          icon: "success",
+        });
+        setDisabledUpdate(true);
+        setCsvData([]);
+        setProducts([]);
+      })
+      .catch((error: any) => {
+        Swal.fire({
+          text: error.response.data.error || "Falha ao atualizar produtos",
+          icon: "warning",
+        });
+      });
+  };
 
   return (
     <>
@@ -95,6 +177,55 @@ function App() {
           >
             Atualizar
           </button>
+        </div>
+      </div>
+
+      <div className="row text-center mt-3 justify-content-center">
+        <div className="col-6">
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th>Código Produto</th>
+                <th>Novo Preço</th>
+                <th>Validação</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products?.length > 0 &&
+                products?.map((product, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>{product.code}</td>
+                      <td>{product.sales_price}</td>
+                      <td>
+                        {product.validation == 0 && (
+                          <button className="button-advice">
+                            <MdWarning color={"#FFBF00"} size={25} />
+                          </button>
+                        )}
+                        {product.validation.error && (
+                          <OverlayTrigger
+                            placement={"top"}
+                            overlay={
+                              <Tooltip>{product.validation.error}</Tooltip>
+                            }
+                          >
+                            <button className="button-advice">
+                              <BiSolidErrorCircle color={"#FF0000"} size={25} />{" "}
+                            </button>
+                          </OverlayTrigger>
+                        )}
+                        {product.validation.success && (
+                          <button className="button-advice">
+                            <AiFillCheckCircle color={"#93c47d"} size={25} />
+                          </button>
+                        )}
+                      </td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
         </div>
       </div>
     </>
